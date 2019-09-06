7ff97f79205c091f3838971fe17aa8e93e9a1a07
Your Name <kavyarajkumar96@gmail.com>
2019-09-06 14:29:40 +0530

    my life first git commit

---
+++ ascii.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+int x="a";
+printf("%l",x);
+return 0;
+}
---
+++ maths.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+       int a=10;
+       int b=5;
+       printf("the sum of two numbers a and b is %d\n ",a+b);
+       printf("the difference of two numbers a and b is %d\n ",a-b);
+       printf("the product of two numbers a and b is %d\n ",a*b);
+       printf("the modulus of two numbers a and b is %d\n ",a/b);
+       return 0;
+}
